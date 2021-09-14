f134b930548913099b89057381ecedc8b900270f
tranvanlamss <lamt63356@gmail.com>
2021-09-14 19:53:05 +0700

    lab1

---
+++ lab1/Pages/Email.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace T2008M_UWP.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Email : Page
+    {
+        public Email()
+        {
+            this.InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string email = inputEmail.Text;
+            string title = inputTitle.Text;
+            string content = inputContent.Text;
+
+            txtEmail.Text = email;
+            txtTitle.Text = title;
+            txtContent.Text = content;
+        }
+    }
+}
